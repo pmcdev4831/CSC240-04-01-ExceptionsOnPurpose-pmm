7da8dbbb9c84377e9351d57533d3df20e66b91dc
mcdevittp <mcdevittp1@mymail.nku.edu>
2024-01-28 17:09:16 -0500

    finished first you do it.

---
+++ ExceptionsOnPurpose.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+
+/*  Author:         Patrick McDevitt
+ *  Date:           1/28/2024
+ *  Description:    You Do IT excercises Chapter 11
+ *  Resources:
+ */
+
+namespace CSC240_04_01_ExceptionsOnPurpose_pmm
+{
+    class ExceptionsOnPurpose
+    {
+        static void Main(string[] args)
+        {
+            // STORAGE
+            int answer;
+            int result;
+            int zero = 0;                  
+            
+            // Input
+            Write("Enter an integer >> ");
+            answer = Convert.ToInt32(ReadLine());
+            // Process
+            result = answer / zero;
+            // Output
+            WriteLine("The answer is " + answer);
+        }
+    }
+}
